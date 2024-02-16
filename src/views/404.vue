4ac2cd244998dcf4a485ee46b723be3f84b3b960
nurafandito <nurafandito@gmail.com>
2024-02-16 19:20:08 +0700

    add 404 page

---
+++ src/views/404.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex items-center justify-center min-h-svh md:min-h-screen">
+    <RouterLink to="/"><img src="https://www.google.com/images/errors/robot.png" /></RouterLink>
+  </div>
+</template>
